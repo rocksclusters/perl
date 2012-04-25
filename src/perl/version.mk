c0c15399388d67ad15f3af6f2c9c9eee92e90109
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2012-04-25 01:59:58 +0000

    Update to latest version of Perl

--- src/perl/version.mk
+++ src/perl/version.mk
@@ -1,8 +1,10 @@
 PKGROOT		= /opt/perl
 NAME    	= opt-perl
 ARCHIVE_NAME	= perl
-VERSION 	= 5.14.1
-PERLVERSION	= 5.014001
+VERSION 	= 5.14.2
+PERLVERSION	= 5.014002
 RELEASE 	= 1
+EXTUTILS_MM	= ExtUtils-MakeMaker
+EXTUTILS_MM_VERSION = 6.62
 TARBALL_POSTFIX	= tar.gz
 RPM.EXTRAS	= AutoReq: no\nProvides: perl(perl) = $(PERLVERSION)
