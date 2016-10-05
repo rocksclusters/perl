e2b3129c32828e19080d40c7e4a3946f97b78dd5
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2016-10-04 17:30:15 -0700

    Update perl version.

--- src/perl/version.mk
+++ src/perl/version.mk
@@ -1,10 +1,10 @@
 PKGROOT		= /opt/perl
 NAME    	= opt-perl
 ARCHIVE_NAME	= perl
-VERSION 	= 5.14.2
+VERSION 	= 5.24.0
 PERLVERSION	= 5.014002
 RELEASE 	= 1
 EXTUTILS_MM	= ExtUtils-MakeMaker
 EXTUTILS_MM_VERSION = 6.62
 TARBALL_POSTFIX	= tar.gz
-RPM.EXTRAS	= AutoReq: no\nProvides: perl(perl) = $(PERLVERSION)
+RPM.EXTRAS	= AutoReq: no\nProvides: opt-perl = $(VERSION)\nProvides: perl(:MODULE_COMPAT_$(VERSION))
